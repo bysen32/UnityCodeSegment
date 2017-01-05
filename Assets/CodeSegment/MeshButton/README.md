b8f2c85d834743c070ee125dd9f6c2f6e961a8d4
bysen32 <bysen32@gmail.com>
2017-01-05 12:05:14 +0800

    Write a Sample of Simple Button

---
+++ Assets/CodeSegment/MeshButton/README.md
@@ -0,0 +1,11 @@
+**The CodeSegment Sample of MeshButton**
+Source code from Unity package - Code Control
+
+* Scripts/Hoverable.cs
+	Check whether the mouse if hover a gameObject.
+	and Change It's state to IsMouseOver.
+* Scripts/MeshButton.cs
+	Need a component of Hoverable, include three state,
+	such as Normal, Hover and Click. define a delegate to callback when
+	Click is occur. achieve animation of Hover and Click by coroutine.
+	
---
+++ Assets/CodeSegment/MeshButton/Scripts/MeshButton.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[RequireComponent(typeof(Hoverable))]
+public class MeshButton : MonoBehaviour {
+    public Action<MeshButton> OnClick;
+
+    public bool IsSelected { get; private set; }
+    public string ButtonName { get { return buttonName; } }
+
+    private enum ButtonState {
+        Normal,
+        Hover,
+        Click,
+    }
+
+    private const float HOVER_SCALE = 1.1f;
+    private const float CLICK_SCALE = 0.9f;
+    private const float CLICK_DURATION = 0.2f;
+
+    private const float SELECT_OFFSET = -.3f;
+    private const float SELECT_DURATIOn = .3f;
+
+    [SerializeField] private string buttonName;
+    [SerializeField] private Transform animationTarget;
+
+    private ButtonState state;
+    private Hoverable hoverable;
+    private Vector3 originalScale;
+    private float scaleFactor;
+
+    private void Awake() {
+        state = ButtonState.Normal;
+        hoverable = GetComponent<Hoverable>();
+        scaleFactor = 1.0f;
+        if (animationTarget == null) animationTarget = transform;
+        originalScale = animationTarget.localScale;
+    }
+    
+    private void Update() {
+        UpdateState();
+    }
+
+    private void UpdateState() {
+        if (hoverable.IsMouseOver && state == ButtonState.Normal) {
+            SetState(ButtonState.Hover);
+        } else if(!hoverable.IsMouseOver && state == ButtonState.Hover) {
+            SetState(ButtonState.Normal);
+        }
+
+        if (Input.GetMouseButtonDown(0) && state == ButtonState.Hover) {
+            SetState(ButtonState.Click);
+        }
+
+        if (state == ButtonState.Click && Input.GetMouseButtonUp(0)) {
+            if (hoverable.IsMouseOver) {
+                if (OnClick != null) {
+                    OnClick(this);
+                }
+                SetState(ButtonState.Hover);
+            } else {
+                SetState(ButtonState.Normal);
+            }
+        }
+    }
+
+    private void SetState(ButtonState state) {
+        this.state = state;
+        StopCoroutine("AnimateClick");
+        StartCoroutine("AnimateClick");
+    }
+
+    private void SetScaleFactor(float factor) {
+        scaleFactor = factor;
+        animationTarget.localScale = originalScale * scaleFactor;
+    }
+
+    private IEnumerator AnimateClick() {
+        float startScale = scaleFactor;
+        float targetScale = 1.0f;
+        switch(state) {
+            case ButtonState.Hover:
+                targetScale = HOVER_SCALE;
+                break;
+            case ButtonState.Click:
+                targetScale = CLICK_SCALE;
+                break;
+        }
+
+        float startTime = Time.time;
+        while (Time.time - startTime < CLICK_DURATION) {
+            SetScaleFactor(startScale + (targetScale - startScale) * (Time.time - startTime) / CLICK_DURATION);
+            yield return new WaitForEndOfFrame();
+        }
+        SetScaleFactor(targetScale);
+    }
+}
